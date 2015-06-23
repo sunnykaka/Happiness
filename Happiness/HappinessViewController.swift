680a03ab547920135e5d17417220d6c0b8d14342
sunnykaka <sunnykaka0721@gmail.com>
2015-06-23 11:02:18 +0800

    init

---
+++ Happiness/HappinessViewController.swift
@@ -0,0 +1,14 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by liubin on 15/6/23.
+//  Copyright (c) 2015年 liubin. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController {
+
+
+}
