7c97ba77713430112122c99cd7d19dba0e294b27
sunnykaka <sunnykaka0721@gmail.com>
2015-06-23 09:45:46 +0800

    Initial Commit

---
+++ Happiness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Happiness
+//
+//  Created by liubin on 15/6/23.
+//  Copyright (c) 2015年 liubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
